7835624ac6b2fad5eae66853d331099271a95cec
FernandoGmz2001 <fernandogmz2001@gmail.com>
2024-06-18 00:32:12 -0500

    feat: Remove unused inputForm component

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,114 +1,121 @@
 "use client";
-import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Switch } from "@/components/ui/switch";
 import { Label } from "@/components/ui/label";
-import { useForm, FormProvider, useFormContext } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
-import { FormControl, FormField, FormItem, FormLabel, FormMessage, Form } from "@/components/ui/form";
-import InputForm  from "@/components/common/inputForm"
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  Form,
+} from "@/components/ui/form";
+import { RedirectType, redirect } from "next/navigation";
 
 const formSchema = z.object({
   email: z.string().email(),
-  password: z.string().min(6,{
-    message: "Password must be at least 6 characters."
-  })
-})
+  password: z.string().min(6, {
+    message: "Password must be at least 6 characters.",
+  }),
+});
 
 const LoginPage = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       email: "",
-      password: ""
-    }
-  })
+      password: "",
+    },
+  });
 
-
-  const login = async (values: z.infer<typeof formSchema>) => {  
-    console.log('caiste en este login')
+  const login = async (values: z.infer<typeof formSchema>) => {
+    try{
       const response = await fetch("/api/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email: values.email, password: values.password}),
-    })
-    const data = await response.json()
-  }
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email: values.email, password: values.password }),
+      });
+      const data = await response.json();
+      redirect("/dashboard", RedirectType.replace)
 
-  const handleSubmit = (values: z.infer<typeof formSchema>) => {
-
-    console.log(values)
-  }
+    }catch(err : any){
+      throw new Error(err.message);
+    }
+  };
 
   return (
     <>
       <main className="w-full p-4">
         <h4 className="font-bold mb-4">Nice to see you again</h4>
         <Form {...form}>
-          <form 
-          onSubmit={form.handleSubmit(login)}
-          className="flex gap-4 w-full flex-col"
+          <form
+            onSubmit={form.handleSubmit(login)}
+            className="flex gap-4 w-full flex-col"
           >
-            <FormField 
-            control={form.control}
-            name="email" 
-            render={({field})=>{
-              return (
-                // <InputForm field={field} placeholder="Email" type="email"/>
-                <FormItem>
-                <FormLabel>Email</FormLabel>
-                <FormControl>
-                  <Input 
-                  placeholder="Email or phone number" 
-                  {...field} 
-                  />
-                </FormControl>
-                <FormMessage/>
-              </FormItem>
-              )
-            }} />
-              <FormField control={form.control} name="password" render={({field})=>{
-              return <FormItem>
-                <FormLabel>Password</FormLabel>
-                <FormControl>
-                  <Input 
-                  placeholder="Enter password" 
-                  type="password"
-                  {...field} 
-                  />
-                </FormControl>
-                <FormMessage/>
-              </FormItem>
-            }} />
-          <div className="flex justify-between text-xs mt-2">
-            <div className="flex items-center space-x-2">
-              <Switch id="airplane-mode" />
-              <Label htmlFor="Remember me" className="text-xs">
-                Remember me
-              </Label>
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Email or phone number" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                );
+              }}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Enter password"
+                        type="password"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                );
+              }}
+            />
+            <div className="flex justify-between text-xs mt-2">
+              <div className="flex items-center space-x-2">
+                <Switch id="airplane-mode" />
+                <Label htmlFor="Remember me" className="text-xs">
+                  Remember me
+                </Label>
+              </div>
+              <a className="text-blue-500 font-medium">Forgot password?</a>
             </div>
-            <a className="text-blue-500 font-medium">Forgot password?</a>
-          </div>
-          <Button className="text-white bg-blue-500 my-2" type="submit">Sign In</Button>
-          <div className="w-full border border-gray-100"></div>
-          <Button className="text-white bg-zinc-800 mt-2">
-            Or sign in with Google
-          </Button> 
+            <Button className="text-white bg-blue-500 my-2" type="submit">
+              Sign In
+            </Button>
+            <div className="w-full border border-gray-100"></div>
+            <Button className="text-white bg-zinc-800 mt-2">
+              Or sign in with Google
+            </Button>
           </form>
         </Form>
-          <div className="w-full flex justify-center mt-4">
-            <p className="text-xs">
-              Don&apos;t have an account?{" "}
-              <span className=" text-blue-500 font-medium">Sign up now</span>
-            </p>
-          </div>
-        {/* </div> */}
+        <div className="w-full flex justify-center mt-4">
+          <p className="text-xs">
+            Don&apos;t have an account?{" "}
+            <span className=" text-blue-500 font-medium">Sign up now</span>
+          </p>
+        </div>
       </main>
     </>
   );
